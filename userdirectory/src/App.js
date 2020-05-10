28b5a047db456a09356030922373f1ec6526cff0
Christopher Wood <cjwood01@gmail.com>
2020-05-09 22:27:06 -0500

    fixed folder paths with components

--- userdirectory/src/App.js
+++ userdirectory/src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MainHeading from './components/Jumbotron';
+import SearchBar from './components/Search';
+import UserList from './components/UserList';
+import UserCard from './components/UserCard';
+import Container from 'react-bootstrap/Container';
+import { Component } from 'react';
+import employeeData from './utils/employeeData.json';
+import Table from 'react-bootstrap/Table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users: employeeData, searchUser: '' };
+  }
+
+  handleInput = e => {
+    console.log(e.target.value);
+    this.setState({ searchUser: e.target.value });
+  };
+
+  render() {
+    let filteredUsers = this.state.users.filter(user => {
+      return user.name
+        .toLowerCase()
+        .includes(this.state.searchUser.toLowerCase());
+    });
+    return (
+      <div className="App">
+        <MainHeading />
+        <SearchBar handleInput={this.handleInput} />
+        <Container>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Phone Number</th>
+                <th>Email</th>
+                <th>D.O.B</th>
+              </tr>
+            </thead>
+          </Table>
+        </Container>
+        <UserList filteredUsers={filteredUsers} />
+        <UserCard />
+      </div>
+    );
+  }
 }
 
 export default App;
