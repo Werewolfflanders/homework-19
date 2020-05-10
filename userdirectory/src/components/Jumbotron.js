c0a3bda8f02a4759248d4f4b8c6639546c6918c9
Christopher Wood <cjwood01@gmail.com>
2020-05-09 21:41:47 -0500

    updated jumotron and search components

---
+++ userdirectory/src/components/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+function MainHeading() {
+  return (
+    <Jumbotron>
+      <h1>Employee Directory</h1>
+      <p>Filter through your employee's to get to know them better!</p>
+    </Jumbotron>
+  );
+}
+
+export default MainHeading;
